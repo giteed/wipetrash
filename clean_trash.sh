918f8f11496978b41577b0dd6fcd9ac1a0d1eefb
Edd <33794654+giteed@users.noreply.github.com>
2025-08-05 15:21:00 +0300

    Update clean_trash.sh

--- clean_trash.sh
+++ clean_trash.sh
@@ -2,6 +2,9 @@
 # clean_trash.sh - основной движок очистки
 # 5.4.8 - 31 Jul 2025
 
+# Проверяет все .sh файлы на наличие прав на выполнение и назначает их, если у какого-либо файла таких прав нет
+for file in *.sh; do [ ! -x "$file" ] && chmod +x *.sh; done
+
 source "$(dirname "${BASH_SOURCE[0]}")/wipe_functions.sh"
 
 WIPE_PASSES="${WIPE_PASSES:-1}"
